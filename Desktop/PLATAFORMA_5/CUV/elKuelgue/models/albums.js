76cfd79d5b6a91cc8b68ee53e5096bc28ec4912a
OSCARJORGERAPP <ojrapp@gmail.com>
2024-10-23 18:46:35 -0300

    CRUD de albumes OK, navegación OK

--- Desktop/PLATAFORMA_5/CUV/elKuelgue/models/albums.js
+++ Desktop/PLATAFORMA_5/CUV/elKuelgue/models/albums.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 
 const albums = new mongoose.Schema({
-    titulo: {type: String, require:true},
-    descripcion:{type:String},
-    fechaDeLanzamiento: {type: String},
-    portada:{type:String},
+    titulo: {type: String, require:true, minlength: 2},
+    descripcion:{type:String,require:true, minlength: 2},
+    fechaDeLanzamiento: {type: String, require:true, minlength: 4},
+    portada: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^(ftp|http|https):\/\/[^ "]+$/.test(v);
+            },
+            message: props => `${props.value} no es una URL válida!`
+        }
+    },
+
     canciones: [{
         nombreDeCancion: { type: String },
         duracion: { type: String }
--- Desktop/PLATAFORMA_5/CUV/elKuelgue/models/users.js
+++ Desktop/PLATAFORMA_5/CUV/elKuelgue/models/users.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 
 const users = new mongoose.Schema({
-    nombre:{type:String, required: true},
-    password:{type:String, required: true},
-    email:{type:String, required:true},
+    nombre:{type:String, required: true, minlength: 2},
+    password:{type:String, required: true, minlength: 6},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    },
     favoritos:[
         {
             album:{type:String},
