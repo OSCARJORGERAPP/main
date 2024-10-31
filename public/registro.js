dc09978e80bd89bab53f80052c2773109aa12484
OSCARJORGERAPP <ojrapp@gmail.com>
2024-10-31 18:41:33 -0300

    Update registro.js

--- public/registro.js
+++ public/registro.js
@@ -1,14 +1,3 @@
-// Validar password > 6 caracteres
-function validarPassword() {
-    const password = document.getElementById('password').value;
-    const mensajeError = document.getElementById('mensajeError');
-    if (password.length < 6) {
-        mensajeError.style.display = 'block';
-    } else {
-        mensajeError.style.display = 'none';
-    }
-}
-
 //GRABA UN USUARIO NUEVO
 // Selecciona el formulario de registro en el documento HTML
 const form = document.getElementById('registrationForm');
